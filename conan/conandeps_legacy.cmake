a9a4ff7cc024a39ae6904240d8038d1b17e79dfa
superfhwl <superfhwl@gmail.com>
2024-05-28 11:55:14 +0800

    - 删除 conan

--- conan/conandeps_legacy.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(gRPC)
-find_package(protobuf)
-
-set(CONANDEPS_LEGACY  grpc::grpc  protobuf::protobuf )
