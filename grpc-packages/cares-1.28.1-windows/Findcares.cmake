5d622b4106162b1c4e3d8cb209533a86504f3776
superfhwl <superfhwl@gmail.com>
2024-05-28 20:06:55 +0800

    + 修改 c-ares 名字为 cares

--- grpc-packages/cares-1.28.1-windows/Findcares.cmake
+++ grpc-packages/cares-1.28.1-windows/Findcares.cmake
@@ -7,27 +7,27 @@
 #
 
 # the following is like an include guard
-set(TARGET_WITH_NAMESPACE "3rdParty::c-ares")
+set(TARGET_WITH_NAMESPACE "3rdParty::cares")
 if (TARGET ${TARGET_WITH_NAMESPACE})
     return()
 endif()
 
 # note that we mimic the behavior or the Findgrpc.cmake that ships with CMake.
 # as such, we declare several variables that other 3rdParty Packages which call
-# find_package(c-ares) might be expecting even if O3DE itself does not use them.
-# this allows the c-ares package we make to be usable as a drop in replacement
-# for other third parties that want to use this c-ares - all you need to do is
+# find_package(cares) might be expecting even if O3DE itself does not use them.
+# this allows the cares package we make to be usable as a drop in replacement
+# for other third parties that want to use this cares - all you need to do is
 # set your CMAKE_MODULE_PATH to the place this package lives and then 
-# calls to find_package(c-ares) will result in this one being used.
+# calls to find_package(cares) will result in this one being used.
 
 # variables required from Findjsbim.cmake in CMake source:
 
 if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(CARES_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR}/c-ares/lib/debug)
+        set(CARES_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR}/cares/lib/debug)
         set(CARES_DLL_DST_DIR ${CMAKE_BINARY_DIR}/bin/Debug)
     else()
-        set(CARES_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR}/c-ares/lib/release)
+        set(CARES_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR}/cares/lib/release)
         set(CARES_DLL_DST_DIR ${CMAKE_BINARY_DIR}/bin/Release)   
     endif()
 
@@ -38,7 +38,7 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 
 endif()
 
-set(CARES_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/c-ares/include)
+set(CARES_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/cares/include)
 set(CARES_INCLUDE_DIR ${CARES_INCLUDE_DIRS})
 
 set(CARES_VERSION_STRING "1.28.1")
@@ -50,7 +50,7 @@ set(CARES_MINOR_VERSION "28")
 set(CARES_PATCH_VERSION "1")
 set(CARES_FOUND True)
 
-# declare the targets - O3DE has the 3rdParty namespace target, CMake has c-ares::c-ares
+# declare the targets - O3DE has the 3rdParty namespace target, CMake has cares::cares
 add_library(${TARGET_WITH_NAMESPACE} INTERFACE IMPORTED GLOBAL)
 
 # cmake < 3.21 and visual studio < 16.10 don't properly implement SYSTEM includes
@@ -91,5 +91,5 @@ endif()
 # A good way to know if you're in O3DE or not is that O3DE sets various cache variables before 
 # calling find_package, specifically, LY_VERSION_ENGINE_NAME is always set very early:
 if (NOT LY_VERSION_ENGINE_NAME)
-    message(STATUS "Using the O3DE version of the c-ares library from ${CMAKE_CURRENT_LIST_DIR}")
+    message(STATUS "Using the O3DE version of the cares library from ${CMAKE_CURRENT_LIST_DIR}")
 endif()
